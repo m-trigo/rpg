4cb6560655b0e4a032cac6d2e8990fe8b3413a6d
Murilo Trigo <murilotrigopaz@gmail.com>
2022-08-18 21:37:42 -0400

    Cleans up the server further

--- server.js
+++ server.js
@@ -2,14 +2,20 @@ const { EOL } = require('os');
 const ws = require('ws')
 const { CLOSED, OPEN } = require('ws');
 
-const port = process.env.PORT || 9021
+const port = process.env.PORT || 5000
 
 let nextClientId = 1;
 
 let webSocketServer = new ws.Server({port});
 webSocketServer.on('connection', client => {
+
     client.id = nextClientId++;
     console.log(`New client connected (id: ${client.id})`);
-    //client.onmessage = event => handlePayload(client, JSON.parse(event.data));
+    
+    client.onmessage = event => {
+        console.log(client);
+        console.log(event);
+        console.log(event.data);
+    }
     //client.send(JSON.stringify({type: 'rooms', data: Object.keys(activeRooms)}));
 });
