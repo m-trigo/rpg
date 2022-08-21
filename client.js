5e39b1dde6235a8598fe6d7c575cae90e8a7add7
Murilo Trigo <murilotrigopaz@gmail.com>
2022-08-21 18:04:35 -0400

    Handles player connections and disconnects

--- client.js
+++ client.js
@@ -2,22 +2,34 @@ const LOCAL = false;
 const serverAddress = LOCAL ? 'ws://localhost:9021' : 'wss://agile-temple-23495.herokuapp.com';
 
 let server = null;
-let cid = -1;
+let cid = 0;
+let pid = 0;
+let clicks = 0;
 
 let screenCentre = s2d.vec.zero;
 let serverOrigin = s2d.vec.zero;
 let players = {};
 
+function player() {
+    return players[pid];
+}
+
+function notify(type, raw = {}) {
+    server.send(JSON.stringify({type, raw}));
+}
+
 function parseServerData(data) {
     if (data.type == 'cid') {
         cid = parseInt(data.raw);
-        console.log(`Joined with id ${cid}`);
-        server.send(JSON.stringify({
-            cid,
-            type: 'init',
-            raw: ''
-        }));
+        console.log(`Joined as client with id ${cid}`);
+        notify('init');
     }
+
+    if (data.type == 'pid') {
+        pid = parseInt(data.raw);
+        console.log(`Joined as player with id ${cid}`);
+    }
+
     if (data.type == 'update') {
         players = data.raw;
     }
@@ -35,23 +47,28 @@ function init() {
 
 function update(dt) {
 
-    if (cid == 0) {
+    if (pid == 0) {
         s2d.canvas.clear('#e1e1e1');
         for (const [pid, player] of Object.entries(players)) {
             let ppos = s2d.vec.add(serverOrigin, player.position);
             let rect = s2d.rect.make(ppos.x, ppos.y, ppos.x + 20, ppos.y + 40);
             s2d.rect.draw(rect, player.color);
         }
+
+        if (s2d.input.mousePressed() && clicks < 3) {
+            clicks++;
+            if (clicks > 2 ) {
+                notify('join');
+            }
+        }
     }
     else {
         if (s2d.input.mouseDown()) {
             let touchPosition = s2d.input.mousePosition();
             let side = touchPosition.x <= screenCentre.x ? 'left' : 'right';
-            server.send(JSON.stringify({
-                cid,
-                type: 'input',
-                raw: side
-            }));
+            let p = player();
+            p.position.x += 120 * dt * (side == 'left' ? - 1 : 1);
+            notify('update', p);
         }
         s2d.canvas.clear('#424242');
         let line = s2d.rect.make(window.innerWidth/2 - 4, 0, window.innerWidth/2 + 4, window.innerHeight);
--- server.js
+++ server.js
@@ -5,51 +5,70 @@ let clients = [];
 let players = {};
 let nextPlayerId = 100;
 
-function broadcast(data) {
-    clients.forEach(c => c.send(JSON.stringify(data)));
+function notify(client, type, raw) {
+    client.send(JSON.stringify({type, raw}));
 }
 
-function playerId(clientId) {
-    let pid = null;
+function broadcast(raw) {
+    clients.forEach(c => notify(c, 'update', raw))
+}
 
+function playerId(cid) {
     for (const [key, value] of Object.entries(players)) {
-        if (value.cid == clientId) {
-            pid = key;
+        if (value.cid == cid) {
+            return value.pid;
         }
     }
+}
 
-    if (pid == null) {
-        pid = nextPlayerId;
-        players[pid] = {
-            cid: clientId,
-            pid,
-            position: { x: 0, y : 0 },
-            color: Math.floor(Math.random() * 16777215).toString(16)
-        };
-        nextPlayerId++;
-    }
-
+function createPlayer(cid) {
+    let pid = nextPlayerId;
+    players[pid] = {
+        cid,
+        pid,
+        position: { x: 0, y : 0 },
+        color: Math.floor(Math.random() * 16777215).toString(16)
+    };
+    nextPlayerId++;
     return pid;
 }
 
-function parseClientData(data) {
-    if (data.type == 'init' && data.cid > 0) {
-        playerId(data.cid);
-        broadcast({type: 'update', raw: players});
+function parseClientData(client, data) {
+    if (data.type == 'init') {
+        notify(client, 'update', players);
     }
 
-    if (data.type == 'input' && data.cid > 0) {
-        let player = players[playerId(data.cid)];
-        player.position.x += 10 * (data.raw == 'left' ? -1 : 1);
-        broadcast({type: 'update', raw: players});
+    if (data.type == 'join') {
+        let pid = createPlayer(client.id);
+        notify(client, 'pid', pid);
+        console.log(`Player joined (pid: ${pid})`)
+        broadcast(players);
+    }
+
+    if (data.type == 'update') {
+        players[playerId(client.id)] = data.raw;
+        broadcast(players);
+    }
+}
+
+function handleDisconnect(cid) {
+    clients = clients.filter(c => c.id != cid)
+    console.log(`Client disconnected (cid: ${cid})`);
+
+    let pid = playerId(cid);
+    if (pid) {
+        delete players[pid];
+        console.log(`Player disconnected (pid ${pid})`);
     }
 }
 
 let webSocketServer = new ws.Server({port});
 webSocketServer.on('connection', client => {
     client.id = clients.length;
-    client.onmessage = message => parseClientData(JSON.parse(message.data));
-    client.send(JSON.stringify({type: 'cid', raw: client.id}));
+    client.onmessage = message => parseClientData(client, JSON.parse(message.data));
+    client.onclose = c => handleDisconnect(c.target.id);
+    notify(client, 'cid', client.id);
+    notify(client, 'update', players);
     clients.push(client);
-    console.log(`Client connected (id: ${client.id})`);
+    console.log(`Client connected (cid: ${client.id})`);
 });
